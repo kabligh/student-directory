a72bef3302a14d1f604ee9a2bc8dbb1e96e85404
K Annabel Bligh <kabligh@gmail.com>
2020-10-22 16:52:49 +0100

    Step 8 exercise 6 – centering the output

--- directory_copy.rb
+++ directory_copy.rb
@@ -25,16 +25,17 @@ def input_students
   students
 end
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]}, from #{student[:country]}, #{student[:cohort]} cohort"
+    puts "#{student[:name]}, from #{student[:country]}, #{student[:cohort]} cohort".center(50)
   end
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "-------------".center(50)
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_students
