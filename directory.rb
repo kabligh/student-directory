54ca8e7c7dd74dc6b59cdd8812a00daf63d2fd89
K Annabel Bligh <kabligh@gmail.com>
2020-10-24 12:36:30 +0100

    Step 13 – taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
@@ -12,14 +12,14 @@ def input_students
     puts "Now we have #{@students.count} students"
     # get another name from the user
     puts "Enter another name"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -66,9 +66,9 @@ def save_students
   file.close
 end
 
-def load_students
+def load_students(filename = "students.csv")
   # open the file for reading
-  file = File.open("students.csv", "r")
+  file = File.open(filename, "r")
   # read all lines into an array and iterate over it
   # discard the new line and split at the comma,
   # assign to the name and cohort variables
@@ -77,6 +77,18 @@ def load_students
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 def print_header
@@ -96,4 +108,5 @@ def print_footer
 end
 
 # nothing happens until we call the methods
+try_load_students
 interactive_menu
