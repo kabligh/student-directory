e1c03dadce1235afd2bf5bf5771f48ff2cdb5cb5
K Annabel Bligh <kabligh@gmail.com>
2020-10-24 12:16:24 +0100

    Step 12 – added method to load previously saved data

--- directory.rb
+++ directory.rb
@@ -27,6 +27,7 @@ def print_menu # extracting the code that prints the menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 bc we'll be adding more items
 end
 
@@ -44,6 +45,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -59,6 +62,19 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  # open the file for reading
+  file = File.open("students.csv", "r")
+  # read all lines into an array and iterate over it
+  # discard the new line and split at the comma,
+  # assign to the name and cohort variables
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
